42abd668643c2e5e9b70a1b3a88fc98612552ffd
shamna pv <shamnapv18@gmail.com>
2025-09-17 19:13:31 +0530

    user details

--- agent/src/app/component/sign-in.tsx
+++ agent/src/app/component/sign-in.tsx
@@ -9,7 +9,7 @@ export default function SignIn() {
         await signIn("github")
       }}
     >
-      <button type="submit">Signin with GitHub</button>
+      <button type="submit" className="px-4 py-2 bg-blue-500 text-white rounded-lg mx-175">Signin with GitHub</button>
     </form>
   )
 } 
--- agent/src/app/page.tsx
+++ agent/src/app/page.tsx
@@ -1,24 +1,28 @@
 import { auth } from "@/auth"
 import SignIn from "@/src/app/component/sign-in"
 import SignOut from "@/src/app/component/sign-out"
-
+import Userdetails from "./component/Userdetails"
+import users from "@/src/app/users/user.json"
 export default async function HomePage() {
   const session = await auth()
 
   return (
-    <main className="flex min-h-screen items-center justify-center">
-      <div className="p-6 border rounded-lg shadow-md">
+    <main >
+      <div >
         {!session ? (
           <>
-            <h1 className="text-2xl font-bold mb-4">Welcome</h1>
+            <h1 className="text-2xl font-bold mb-4 text-center py-50">Welcome</h1>
             <SignIn />
           </>
         ) : (
           <>
-            <h1 className="text-2xl font-bold mb-4">
-              Hello, {session.user?.name}
-            </h1>
-            <SignOut />
+            <div className="flex items-center justify-between mb-7">
+              <h1 className="text-2xl font-bold">
+                Hello, {session.user?.name}
+              </h1>
+              <SignOut/>
+            </div>
+            <Userdetails user={users} />
           </>
         )}
       </div>
